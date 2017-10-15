f0ad60421d78b1d050e268fb93e84cc993f37873
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2017-10-15 06:54:22 -0400

    more work

--- six/app.js
+++ six/app.js
@@ -51,12 +51,14 @@ app.use(function(err, req, res, next) {
 });
 var retweet = function(){
   var params = {
-    q:'#masturbate, #nsfw',
+    // q:'#nudes, #nsfw',
+    q:'#selfie',
     count:10,
     result_type:'recent',
     lang:'en'
   }
   T.get('search/tweets', params, function(err, data, response){
+    var urlz = [];
     if(!err){
       for(let i = 0; i < data.statuses.length; i++){
         let id = {id:data.statuses[i].id_str};
@@ -66,14 +68,34 @@ var retweet = function(){
           }else{
             let username = response.user.screen_name;
             let tweetId = response.id_str;
-            console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`);
+            let url = `https://twitter.com/${username}/status/${tweetId}`;
+            // console.log('Favorit: ',url);
+
+            urlz.push(url);
+
           }
         });
       }
     }else{
       console.log(err);
     }
+
   });
 }
-retweet();
+
+// retweet();
+
+var links =[
+  'https://twitter.com/Matasnet/status/919506462048772096',
+  'https://twitter.com/ChiomaBanks/status/919505996942336000',
+  'https://twitter.com/ANTONYBRADDON/status/919505292232089600',
+  'https://twitter.com/Magidas1/status/919505893238063105',
+  'https://twitter.com/first4edu/status/919507046407593984',
+  'https://twitter.com/gbramwellxo/status/919507092339396609',
+  'https://twitter.com/IAmNotAFakeGirl/status/919505647498137600',
+  'https://twitter.com/Pikagamergirl/status/919506780438319105',
+  'https://twitter.com/Bun_Bun31/status/919505295923204096',
+  'https://twitter.com/Xhakaal/status/919506878387822593'
+];
+res.send(links);
 module.exports = app;
